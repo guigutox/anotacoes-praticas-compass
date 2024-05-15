0ae2714b270db7ac56b4cccdff83ada4a93f6e2a
Guilherme <guilhermegutofer@gmail.com>
2024-05-15 12:56:03 -0300

    Se aplicado, esse commit ira adicionar a conexão com o bbanco de dados, alem da configuração para criação da tabela perguntas e pagina ejs de perguntas funcionando

--- Node-Curso/P1_PlataformaPerguntasRespostas/index.js
+++ Node-Curso/P1_PlataformaPerguntasRespostas/index.js
@@ -1,21 +1,47 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database.js");
+const Pergunta = require("./database/Pergunta.js");
+
+//Conectando ao banco de dados
+
+connection.authenticate().then(() => {
+  console.log("Conectado com sucesso ao banco de dados!");
+});
 
 //Declarando o EJS como view engine
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+//Configurando o body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Configurando as rotas
 app.get("/", function (req, res) {
-    res.render("index");
+  Pergunta.findAll({raw : true}).then((perguntas) => {
+    res.render("index",{
+      perguntas: perguntas
+    });
+  });
+  
 });
 
 app.get("/perguntar", function (req, res) {
-    res.render("perguntar");
+  res.render("perguntar");
 });
 
-app.get("/salvarpergunta", (req, res) => {
-  
+app.post("/salvarpergunta", (req, res) => {
+  let titulo = req.body.titulo;
+  let descricao = req.body.descricao;
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao,
+  }).then(() => {
+    res.redirect("/");
+  });
 });
 
 app.listen(3000, () => {
