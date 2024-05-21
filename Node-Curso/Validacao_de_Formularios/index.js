aa07a7f8b780013befc9667f2277cd431e5cb4d4
Guilherme <guilhermegutofer@gmail.com>
2024-05-21 10:26:43 -0300

    Se aplicado, esse commit irá adicionar as modificações finais para validacao de formularios no diretorio Validacao_de_Formularios

--- Node-Curso/Validacao_de_Formularios/index.js
+++ Node-Curso/Validacao_de_Formularios/index.js
@@ -1,63 +1,78 @@
-const bodyParser = require("body-parser");
-const express = require("express");
-const session = require("express-session");
-const flash = require("express-flash");
-const app = express();
-const cookieParser = require("cookie-parser");
+var express = require("express");
+var app = express();
+var session = require("express-session");
+var flash = require("express-flash");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser("keyboard cat"));
-app.use(
-  session({
-    secret: "keyboard cat",
+app.set('view engine','ejs');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(cookieParser("jsaddsh"));
+app.use(session({
+    secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 1000000000 },
-  })
-);
+    cookie: { maxAge: 60000 }
+  }))
 
 app.use(flash());
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+
+app.get("/",(req,res) => {
+
+    var emailError = req.flash("emailError");
+    var pontosError = req.flash("pontosError");
+    var nomeError = req.flash("nomeError");
+    var email = req.flash("email");
+
+    emailError = (emailError == undefined || emailError.length == 0) ? undefined : emailError;
+    email = (email == undefined || email.length == 0) ? "" : email;
+
+    res.render("index",{emailError,pontosError,nomeError,email: email });
+})
+
+app.post("/form",(req, res) => {
+    var {email, nome, pontos} = req.body;
+
+    var emailError;
+    var pontosError;
+    var nomeError;
+
+    if(email == undefined || email == ""){
+        emailError = "O e-mail não pode ser vazio";
+    }
+
+    if(pontos == undefined || pontos < 20){
+        pontosError = "Você não pode ter menos de 20 ponto";
+    }
+
+    if(nome == undefined || nome == ""){
+        nomeError = "O nome não pode ser vazio";
+    }
+
+    if(nome.length < 4){
+        nomeError = "O nome é mt pequeno";
+    }
+
+    if(emailError != undefined || pontosError != undefined || nomeError != undefined){
+        req.flash("emailError",emailError);
+        req.flash("pontosError",pontosError);
+        req.flash("nomeError",nomeError);
+
+        req.flash("email",email);
+
+        res.redirect("/");
+    }else{
+        res.send("SHOW DE BOLA ESSE FORM!");
+    }
 });
 
-app.post("/form", (req, res) => {
-  let { email, nome, pontos } = req.body;
-
-  let emailError;
-  let pontosError;
-  let nomeError;
-
-  if (email == undefined || email == "") {
-    emailError = "Email obrigatório";
-  }
-
-  if (pontos == undefined || pontos < 20) {
-    pontosError = "Pontos obrigatórios e deve ser maior que 20";
-  }
-
-  if (nome == undefined || nome == "") {
-    nomeError = "Nome obrigatório";
-  }
-
-  if (
-    emailError != undefined ||
-    pontosError != undefined ||
-    nomeError != undefined
-  ) {
-    res.redirect("/");
-  } else {
-    req.flash("email", email);
-    req.flash("pontos", pontos);
-    req.flash("nome", nome);
-  }
-
-  res.send(email);
-});
-
-app.listen(3000, (req, res) => {
-  console.log("Servidor iniciado na porta 3000");
+app.listen(3000,(req, res) => {
+    console.log("Servidor rodando na porta 3000");
 });
