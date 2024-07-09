afff1f74cdddf1054667e386dec67c3fe85c01cc
Guilherme <guilhermegutofer@gmail.com>
2024-07-09 15:59:44 -0300

    Se aplicado, esse commit irá adicionar as atividades realizadas no módulo 4 do curso de solidity

---
+++ SmartContracts-Praticas/Módulo 4- Deposit and Withdrawals/MyContract.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+
+contract MyContract{
+
+    string public ourString = "Hello World";
+
+
+    function updateOurString(string memory _updateString) public {
+        ourString = _updateString;
+
+    }
+
+
+}
